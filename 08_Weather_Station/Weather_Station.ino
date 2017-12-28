83fc76f2a7819e2540d3c8bafb2adbedc167d750
userdw <insidetrainingie@gmail.com>
2017-12-28 14:29:34 +0700

    Add files via upload

---
+++ 08_Weather_Station/Weather_Station.ino
@@ -0,0 +1,13 @@
+float temp;
+
+void setup() {
+  Serial.begin(57600);
+};
+
+void loop () {
+  temp = analogRead(A0)*1000/1023.0;
+  temp = temp - 500;
+  temp = temp / 10;
+  Serial.println(temp);
+  delay(500);
+};
