b7af9a1064d52a2912b09278879fa9bd7ec0107a
userdw <insidetrainingie@gmail.com>
2017-12-28 14:28:56 +0700

    Add files via upload

---
+++ 06_Potentiometer_Controlled_Servo/Potentiometer_Controlled_Servo.ino
@@ -0,0 +1,18 @@
+#include <Servo.h>
+#define SERVO 4
+#define ROTATION_SENSOR A0
+
+Servo sg90;
+
+void setup() {
+  sg90.attach(SERVO);
+  Serial.begin(9600);
+}
+
+void loop() {
+  int rotationData = analogRead(A0);
+  rotationData = map(rotationData, 0, 1023, 0, 255);
+  sg90.write(rotationData);
+  Serial.println(A0);
+  Serial.print(rotationData);
+}
