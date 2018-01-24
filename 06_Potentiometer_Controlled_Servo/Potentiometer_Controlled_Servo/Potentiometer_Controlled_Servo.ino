c4a2bb28dbd68152ba4d334dbe93cb84e6781fc0
userdw <insidetrainingie@gmail.com>
2018-01-24 16:32:02 +0700

    Update Potentiometer_Controlled_Servo.ino

--- 06_Potentiometer_Controlled_Servo/Potentiometer_Controlled_Servo/Potentiometer_Controlled_Servo.ino
+++ 06_Potentiometer_Controlled_Servo/Potentiometer_Controlled_Servo/Potentiometer_Controlled_Servo.ino
@@ -1,1 +1,26 @@
+/************PIN DECLARATION FOR ESP8266 I/O EXPANSION SHIELD************/
+        //Shield  ESP8266//
+#define EXP_PIN_2     0
+#define EXP_PIN_3     4  
+#define EXP_PIN_4     5
+#define EXP_PIN_5     12
+#define EXP_PIN_6     13  
+#define EXP_PIN_7     15 //SERVO
+/************PIN DECLARATION FOR ESP8266 I/O EXPANSION SHIELD************/
+
+#include <Servo.h>
+#define EXP_PIN_7 6
+#define ROTATION_SENSOR A0
+
+Servo sg90;
+
+void setup() {
+  sg90.attach(EXP_PIN_7);
+}
+
+void loop() {
+  int rotationData = analogRead(A0);
+  rotationData = map(rotationData, 0, 1023, 0, 180);
+  sg90.write(rotationData);
+}
 
