184353520ae0f24ccb7e005390aee2637e534772
userdw <insidetrainingie@gmail.com>
2018-01-30 10:03:08 +0700

    Update Mario_Bros-s_Tone_Generator.ino

--- 07_Mario_Bros-s_Tone_Generator/Mario_Bros-s_Tone_Generator/Mario_Bros-s_Tone_Generator.ino
+++ 07_Mario_Bros-s_Tone_Generator/Mario_Bros-s_Tone_Generator/Mario_Bros-s_Tone_Generator.ino
@@ -1,12 +1,15 @@
-/************PIN DECLARATION FOR ESP8266 I/O EXPANSION SHIELD************/
-        //Shield  ESP8266//
+/*------------PIN DECLARATION FOR ESP8266 I/O EXPANSION SHIELD------------
+        I/O         ORIGINAL
+        Expansion     PIN
+        Shield      ESP8266
+--------------------------------------------------------------------------*/
 #define EXP_PIN_2     0
 #define EXP_PIN_3     4  
 #define EXP_PIN_4     5
 #define EXP_PIN_5     12 //BUZZER MODULE
 #define EXP_PIN_6     13  
 #define EXP_PIN_7     15 
-/************PIN DECLARATION FOR ESP8266 I/O EXPANSION SHIELD************/
+/*------------PIN DECLARATION FOR ESP8266 I/O EXPANSION SHIELD------------*/
 
 /*
   Arduino Mario Bros Tunes
